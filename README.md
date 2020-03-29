891542562d9c7ad023dd630f8cbda0c63dcae760
Trevor Youngquist <46886041+ytrevor81@users.noreply.github.com>
2020-03-29 14:06:41 +0700

    delete original test bot

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A Telegram Bot that automatically shares data of changes in Bitcoin
 
 # Command Line
 - pip install pyTelegramBotAPI
-- ..../correct_dir python bot.py
+- ..../FeistyBot python bot.py
--- bot.py
+++ bot.py
@@ -3,25 +3,27 @@
 import telebot  #imports pyTelegramBotAPI
 import time     #we need this later error handling
 
-bot_token = "1077672229:AAF748sRH3irmCoraBVkIyXg8lACTQEyttY"
+bot_token = "935833055:AAGKz1k1ICpnZCveQ648TVLabUmN5QDWWa4"
 
 bot = telebot.TeleBot(token=bot_token) #Bot Object
 
 @bot.message_handler(commands=['start'])    #wrapper for all functions below
 def send_welcome(message):
     '''This will send a message on the command /start'''
-    bot.reply_to(message, "Welcome to Feisty Fern's test bot! Use the command /help for more info") #this takes 2 parameters--the command /start and the bot's desired reply
+    bot.reply_to(message, "Welcome to THE Feisty Fern test bot! Use the command /help for more info") #this takes 2 parameters--the command /start and the bot's desired reply
 
 @bot.message_handler(commands=['help'])    #wrapper for all functions below
 def send_welcome(message):
     '''This will send a message on the command /help'''
-    bot.reply_to(message, "TEST")
+    bot.reply_to(message, "Testing underway")
 
 
 
+
+bot.polling() #activates the connection between the script and the bot
 ###Telegram will try to cancel this connection every hour. This will temporarily stop it from shutting down
-while True:
-    try:
-        bot.polling() #activates the connection between the script and the bot
-    except Exception:
-        time.sleep(15)
+#while True:
+#    try:
+#        bot.polling() #activates the connection between the script and the bot
+#    except Exception:
+#        time.sleep(15)
